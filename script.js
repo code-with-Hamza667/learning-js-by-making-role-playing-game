c8bf7c7f86b7186f7c54b026b127cd92b4c35f3f
Hamza Khatri <116482376+code-with-Hamza667@users.noreply.github.com>
2024-01-06 16:22:07 +0500

    script setting updated

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let xp = 0;
 let health = 100;
 let gold = 50;
 let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ['stick','dagger','sword'];
+
+let button1 = document.querySelector('#button1+');
